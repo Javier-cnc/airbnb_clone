1baeba10c7a089d1958436ce3f0238698c0f0244
Javier-cnc <grumpyjg95@gmail.com>
2023-01-18 21:48:39 -0500

    Complete carousel implementation

--- src/app/awesome-carousel/awesome-carousel.component.ts
+++ src/app/awesome-carousel/awesome-carousel.component.ts
@@ -19,6 +19,9 @@ export class AwesomeCarouselComponent {
   @ViewChild('itemsContainer')
   itemsContainer!: ElementRef;
 
+  @ViewChild('scrolleableIndicator')
+  scrolleableIndicator!: ElementRef;
+
   @Input('areActionButtonsVisible')
   areActionButtonsVisible: boolean = false;
 
@@ -37,6 +40,10 @@ export class AwesomeCarouselComponent {
 
   // indicates which element of 'values' collection is being showed in the carousel
   private currentIndex: number = 0;
+
+  // indicates the same value as currentIndex but its value is updated after
+  // the smooth scroll animation of the carousel
+  public animatedCurrentIndex: number = 0;
 
   get CurrentIndex() {
     return this.currentIndex;
@@ -57,7 +64,7 @@ export class AwesomeCarouselComponent {
 
     this.currentIndex = value;
 
-    // #region Scroll operation
+    // #region Scroll operation for main container
 
     var containerElement = this.itemsContainer.nativeElement;
 
@@ -70,11 +77,65 @@ export class AwesomeCarouselComponent {
     // perform the scroll operation of the parent element
     containerElement.scrollLeft = offset;
 
+    containerElement.addEventListener();
+
+    // #endregion
+    // note: the indicator will be scrolled after the smooth scroll operation has finished
+    // see handler for scroll event in ngAfterViewInit() method...
+  }
+
+  private scrollIndicator() {
+    // #region Scroll operation for indicators container
+    // get the reference to the indicators container
+    var parentIndicator = this.scrolleableIndicator.nativeElement;
+    var childIndicator = parentIndicator.children[this.currentIndex];
+
+    // === get the width for the parent and the child ===
+    var parentWidth = parentIndicator.offsetWidth;
+    console.log(`ParentWidth: ${parentWidth}`);
+
+    var childWidth = childIndicator.offsetWidth;
+    console.log(`ChildWidth: ${childWidth}`);
+
+    // === calculate the space required between the left border of the parent and
+    // the left border of the child to garantee the child will be in the middle of the
+    // parent view port ===
+    var space = parentWidth / 2 - childWidth / 2;
+    console.log(`space: ${space}`);
+
+    var scrollPosition = childIndicator.offsetLeft - space;
+    console.log(`offsetLeft: ${childIndicator.offsetLeft}`);
+    console.log(`scrollPosition: ${scrollPosition}`);
+
+    parentIndicator.scrollLeft = scrollPosition;
+
     // #endregion
   }
 
   ngAfterViewInit(): void {
-    // this.executeTestingCode();
+    // #region handler for the scroll event of indicator container
+    let timer: any;
+    // the reason for this handler is because there is not any event
+    // fired when the animation of the smooth scrolling operation ends
+    // so the system I will use 'scroll' event of the element to start a timming
+    // function which is reset in the meanwhile the event is still being fired
+    // when the event stop being fired for an ammount of time I will execute the callback()
+    // assuming that the scrolling animation has ended
+    this.itemsContainer.nativeElement.addEventListener(
+      'scroll',
+      () => {
+        clearTimeout(timer);
+
+        timer = setTimeout(() => {
+          this.scrollIndicator();
+          this.animatedCurrentIndex = this.currentIndex;
+        }, 100);
+      },
+      { passive: true }
+    );
+    // #endregion
+
+    //this.executeTestingCode();
   }
 
   private executeTestingCode() {
